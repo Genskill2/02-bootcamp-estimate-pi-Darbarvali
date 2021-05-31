fe8736018cdc9a772767cad9f9cf58c4a0ec8869
Darbarvali <darbar_b190690me@nitc.ac.in>
2021-05-31 14:41:40 +0530

    added wallis and monte_carlo functions

--- estimate.py
+++ estimate.py
@@ -1,5 +1,26 @@
-import math
+import math   
 import unittest
+import random
+
+
+def wallis(n):
+    dummy=1
+    for i in range (1,n+1):
+      dummy*= ((4)*(i**2))/(((4)*(i**2))-1)
+    return dummy*2
+    
+         
+def monte_carlo(n):
+  count = 0
+  for i in range(0, n):
+    x = random.random()
+    y = random.random()
+    if ((x**2 + y**2)**0.5) <= 1 :
+      count+= 1
+  
+  return 4*(count)/n
+
+
 
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
